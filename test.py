320b77910aa31787e1d289b7402c33a5dd60a0dc
spline <reticulatingspline@github.com>
2014-10-17 06:53:40 -0400

    Edit test.py

--- test.py
+++ test.py
@@ -11,6 +11,6 @@ class WebHooksTestCase(PluginTestCase):
     plugins = ('WebHooks',)
     
     def testWebHooks(self):
-        pass# New York, NY, USA :: Current local time is: Thu, 12:02 (Eastern Daylight Time)
+        pass
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
