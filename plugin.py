dd6f7d28f05af6da4825aa70440aded067f6fe05
spline <reticulatingspline@github.com>
2014-10-17 18:35:12 -0400

    Internal updates.

--- plugin.py
+++ plugin.py
@@ -28,113 +28,116 @@ except ImportError:
     # without the i18n module
     _ = lambda x:x
 
-
-####################
-# HELPER FUNCTIONS #
-####################
-
+# need this for logging.
 logg = log.getPluginLogger('WebHooks')
 
-def _r(s):
-    return ircutils.mircColor(s, 'red')
+########################
+# FORMATTING FUNCTIONS #
+########################
 
-def _y(s):
-    """Returns a yellow string."""
-    return ircutils.mircColor(s, 'yellow')
-
-def _g(s):
-    """Returns a green string."""
-    return ircutils.mircColor(s, 'green')
-
-def _b(s):
-    """Returns a blue string."""
-    return ircutils.mircColor(s, 'blue')
-
-def _lb(s):
-    """Returns a light blue string."""
-    return ircutils.mircColor(s, 'light blue')
-
-def _o(s):
-    """Returns an orange string."""
-    return ircutils.mircColor(s, 'orange')
-
-def _bold(s):
-    """Returns a bold string."""
-    return ircutils.bold(s)
-
-def _ul(s):
-    """Returns an underline string."""
-    return ircutils.underline(s)
-
-def _bu(s):
-    """Returns a bold/underline string."""
-    return ircutils.bold(ircutils.underline(s))
-
-def flatten_subdicts(dicts, flat=None):
-    """Change dict of dicts into a dict of strings/integers. Useful for
-    using in string formatting."""
-    if flat is None:
-        # Instanciate the dictionnary when the function is run and now when it
-        # is declared; otherwise the same dictionnary instance will be kept and
-        # it will have side effects (memory exhaustion, ...)
-        flat = {}
-    if isinstance(dicts, list):
-        return flatten_subdicts(dict(enumerate(dicts)))
-    elif isinstance(dicts, dict):
-        for key, value in dicts.items():
-            if isinstance(value, dict):
-                value = dict(flatten_subdicts(value))
-                for subkey, subvalue in value.items():
-                    flat['%s__%s' % (key, subkey)] = subvalue
-            else:
-                flat[key] = value
-        return flat
-    else:
-        return dicts
+class Formatting():
+    """Class for formatting."""
     
-##############
-# FORMATTING #
-##############
-
-def format_push(d):
-    """Format a push for IRC."""
+    def __init__(self):
+        self.log = log.getPluginLogger('WebHooks')
     
-    try:
-        # [GitPull] reticulatingspline pushed 1 commit to master [+0/-0/1] http://git.io/n4lbSQ
-        # [GitPull] spline e2070d1 - Fix initial test as I forgot it might actually update.
-        #repoowner = d['repository__owner__name']
-        reponame = d['repository__name']
-        commit_msg = d['head_commit__message']
-        committer = d['commits'][0]['committer']['name']
-        numofc = len(d['commits'])
-        branch = d['repository__master_branch']
-        compare = d['compare']
-        m = "[{0}] {1} pushed {2} commit(s) to {3} {4} {5}".format(_b(reponame),\
-                                                                   _r(committer),\
-                                                                   _bold(numofc),\
-                                                                   _o(branch),\
-                                                                   commit_msg,\
-                                                                   compare)
-        return (reponame, m)
-    except Exception as e:
-        log.info("_format_push :: ERROR :: {0}".format(e))
-        return None
+    def _r(self, s):
+        return ircutils.mircColor(s, 'red')
+    
+    def _y(self, s):
+        """Returns a yellow string."""
+        return ircutils.mircColor(s, 'yellow')
+    
+    def _g(self, s):
+        """Returns a green string."""
+        return ircutils.mircColor(s, 'green')
+    
+    def _b(self, s):
+        """Returns a blue string."""
+        return ircutils.mircColor(s, 'blue')
+    
+    def _lb(self, s):
+        """Returns a light blue string."""
+        return ircutils.mircColor(s, 'light blue')
+    
+    def _o(self, s):
+        """Returns an orange string."""
+        return ircutils.mircColor(s, 'orange')
+    
+    def _bold(self, s):
+        """Returns a bold string."""
+        return ircutils.bold(s)
+    
+    def _ul(self, s):
+        """Returns an underline string."""
+        return ircutils.underline(s)
+    
+    def _bu(self, s):
+        """Returns a bold/underline string."""
+        return ircutils.bold(ircutils.underline(s))
 
-def format_status(d):
-    """Format."""
-    try:
-        # [Assorted] Travis CI - build #73 passed. (master @ 3c4572b) http://git.io/OhYANw
-        # [Assorted] Details: https://travis-ci.org/reticulatingspline/Assorted/builds/38050581
-        reponame = d['repository__name']
-        branch = d['branches'][0]['name']  # branch.
-        sha = d['branches'][0]['commit']['sha'][0:7]  # first 7 of the sha.
-        desc = d['description']  # "state": "pending"
-        target_url = d['target_url']
-        m = "[{0}] {1} - ({2}@{3}) {4}".format(_b(reponame), _bold(desc), branch, sha, target_url)
-        return (reponame, m)
-    except Exception as e:
-        log.info("format_status :: ERROR :: {0}".format(e))
-        return None
+    def flatten_subdicts(self, dicts, flat=None):
+        """Change dict of dicts into a dict of strings/integers. Useful for
+        using in string formatting."""
+        if flat is None:
+            # Instanciate the dictionnary when the function is run and now when it
+            # is declared; otherwise the same dictionnary instance will be kept and
+            # it will have side effects (memory exhaustion, ...)
+            flat = {}
+        if isinstance(dicts, list):
+            return flatten_subdicts(dict(enumerate(dicts)))
+        elif isinstance(dicts, dict):
+            for key, value in dicts.items():
+                if isinstance(value, dict):
+                    value = dict(flatten_subdicts(value))
+                    for subkey, subvalue in value.items():
+                        flat['%s__%s' % (key, subkey)] = subvalue
+                else:
+                    flat[key] = value
+            return flat
+        else:
+            return dicts
+
+    def format_push(self, d):
+        """Format a GitHub push for IRC."""
+        
+        try:
+            # [GitPull] reticulatingspline pushed 1 commit to master [+0/-0/1] http://git.io/n4lbSQ
+            # [GitPull] spline e2070d1 - Fix initial test as I forgot it might actually update.
+            #repoowner = d['repository__owner__name']
+            reponame = self._b(d['repository__name'])
+            commit_msg = d['head_commit__message']
+            committer = self._r(d['commits'][0]['committer']['name'])
+            numofc = self._bold(len(d['commits']))
+            branch = self._o(d['repository__master_branch'])
+            compare = d['compare']
+            m = "[{0}] {1} pushed {2} commit(s) to {3} {4} {5}".format(reponame,\
+                                                                       committer,\
+                                                                       numofc,\
+                                                                       branch,\
+                                                                       commit_msg,\
+                                                                       compare)
+            return (reponame, m)
+        except Exception as e:
+            log.info("_format_push :: ERROR :: {0}".format(e))
+            return None
+
+    def format_status(self, d):
+        """Format GitHub status messages like from Travis-CI."""
+        
+        try:
+            # [Assorted] Travis CI - build #73 passed. (master @ 3c4572b) http://git.io/OhYANw
+            # [Assorted] Details: https://travis-ci.org/reticulatingspline/Assorted/builds/38050581
+            reponame = self._b(d['repository__name'])
+            branch = d['branches'][0]['name']  # branch.
+            sha = d['branches'][0]['commit']['sha'][0:7]  # first 7 of the sha.
+            desc = self._bold(d['description'])  # "state": "pending"
+            target_url = d['target_url']
+            m = "[{0}] {1} - ({2}@{3}) {4}".format(reponame, desc, branch, sha, target_url)
+            return (reponame, m)
+        except Exception as e:
+            log.info("format_status :: ERROR :: {0}".format(e))
+            return None
 
 class WebHooksServiceCallback(httpserver.SupyHTTPServerCallback):
     """
@@ -145,7 +148,7 @@ class WebHooksServiceCallback(httpserver.SupyHTTPServerCallback):
     defaultResponse = """This plugin handles only POST request, please don't use other requests."""
     
     def __init__(self):
-        self._log = log.getPluginLogger('WebHooks')
+        self.log = log.getPluginLogger('WebHooks')
         
     def doPost(self, handler, path, form):
         log.info("{0}".format(handler.address_string()))
@@ -178,10 +181,9 @@ class WebHooksServiceCallback(httpserver.SupyHTTPServerCallback):
             # good payload so lets process it.
             json_payload = form.getvalue('payload')  # take from the form.
             payload = json.loads(json_payload)  # json -> dict.
-            d = flatten_subdicts(payload)  # flatten it out.
-            
+            d = Formatting.flatten_subdicts(payload)  # flatten it out.
             #self._log.info("doPost: {0}".format(d))  # log the message.
-            logg.info("doPost: {0}".format(d))  # log the message.
+            self.log.info("doPost: {0}".format(d))  # log the message.
             # lets figure out how to handle each type of notification here.
             # https://developer.github.com/webhooks/
             if headers['x-github-event'] == 'push':  # push event.
@@ -215,22 +217,6 @@ class WebHooks(callbacks.Plugin):
         self.__parent.die()
         httpserver.unhook('webhooks')
 
-    #######################
-    # WEB HOOK ANNOUNCING #
-    #######################
-
-    def announce_webhook(self, repo, message):
-        """Internal function to announce webhooks."""
-        
-        # lower it first.
-        repo = repo.lower()
-        # only work if present
-        if repo in self._webhooks:  # if represent present.
-            for c in self._webhooks[repo]:  # for each chan in it.
-                for irc in world.ircs:  # all networks.
-                    if c in irc.state.channels:  # if channel matches.
-                        irc.queueMsg(ircmsgs.privmsg(c, message))  # post.
-
     #####################
     # INTERNAL DATABASE #
     #####################
@@ -263,6 +249,22 @@ class WebHooks(callbacks.Plugin):
         except IOError:
             return False
         return True
+
+    #######################
+    # WEB HOOK ANNOUNCING #
+    #######################
+
+    def announce_webhook(self, repo, message):
+        """Internal function to announce webhooks."""
+        
+        # lower it first.
+        repo = repo.lower()
+        # only work if present
+        if repo in self._webhooks:  # if represent present.
+            for c in self._webhooks[repo]:  # for each chan in it.
+                for irc in world.ircs:  # all networks.
+                    if c in irc.state.channels:  # if channel matches.
+                        irc.queueMsg(ircmsgs.privmsg(c, message))  # post.
 
     ############################
     # PUBLIC DATABASE COMMANDS #
